d935bb885fd40312a762590abeeca997029cd9bf
Ali Ashtiani <ali.ashtiani24@gmail.com>
2016-06-04 10:01:43 -0700

    Xcode Environment Setup

---
+++ EasyCalc/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EasyCalc
+//
+//  Created by Ali Ashtiani on 6/4/16.
+//  Copyright © 2016 Ali Ashtiani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Welcome to EasyCalc");
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
